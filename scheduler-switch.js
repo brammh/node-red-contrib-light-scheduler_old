4f21dd77ccdae25032825c465db082da79e812cf
Bram Hoefnagel <bram.hoefnagel@signify.com>
2019-02-28 23:03:24 +0100

    Fixed init bugg - mssg every 60s gone now

--- scheduler-switch.js
+++ scheduler-switch.js
@@ -61,7 +61,15 @@ module.exports = function(RED) {
                     newMsg.payload = RED.util.evaluateNodeProperty(node.offPayload, node.offPayloadType, node, newMsg);
             }
             
-            node.warn(`out:${out} - prevState:${node.prevState} - newPayload:${newMsg.payload} prevPayload:${node.prevPayload}`);
+            //Populate the previous states if they are not filled yet
+            if(node.prevState==null||node.prevPayload==null)
+            {
+                node.prevPayload = newMsg.payload;
+                node.prevState = out;
+            }
+            //FOR DEBUGGING DISABLE BELOW
+            //node.warn(`out:${out} - prevState:${node.prevState} - newPayload:${newMsg.payload} prevPayload:${node.prevPayload}`);
+
             // Only send anything if the state has changed.
             if(out!=node.prevState||newMsg.payload!==node.prevPayload)
             {
